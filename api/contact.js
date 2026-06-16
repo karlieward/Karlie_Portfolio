185053d7190294f2def3a578c1051cd6391e540d
Karlie <karlie3@byu.edu>
2026-06-16 13:57:00 -0600

    Use verified SendGrid email address

--- api/contact.js
+++ api/contact.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
     // Prepare email content
     const msg = {
       to: 'karlie3@byu.edu',
-      from: 'noreply@karlie.dev',
+      from: 'karlie3@byu.edu',
       replyTo: email,
       subject: `New Contact Form Submission from ${name}`,
       html: `
